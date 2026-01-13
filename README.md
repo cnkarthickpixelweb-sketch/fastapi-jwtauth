0d21d19be07998751e6db8e8ce4587d35d591991
KarthickCN <cnkarthick@pixelwebsolutions.net>
2026-01-13 17:28:46 +0530

    demo sql file and  .env.example file added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # fastapi-jwtauth
 Fast Api Jwt using create user, update profile, authorization source
+
+# Step 1 :
+    python3 -m venv venv
+
+# Step 2: 
+    source venv/bin/activate
+
+# Step 3:
+    pip install -r requirements.txt
+
+# step 4:
+    mysql or psql import your local 
+
+# step 5:
+    .env your secrect key and db crendentials added
+
+# step 6: 
+    run: uvicorn app.main:app --reload
+
+# step 7:
+    http://127.0.0.1:8000/docs
---
+++ mysql/fastapi_jwt.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 13, 2026 at 12:49 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fastapi_jwt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(155) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_image` varchar(255) DEFAULT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `profile_image`, `created_at`, `updated_at`) VALUES
+(9, 'teddy', 'cup', 'teddycup@mailinator.com', '$argon2id$v=19$m=65536,t=3,p=4$43yvtXZOSUlJiTGGMMZ4rw$ymbbVmqylFtm3TJUiUfBEdfQnBUuHg6Ei15WvMusFnw', 'profile_images/kohli.jpeg', '2026-01-12', '2026-01-12 11:55:59');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
